ee50bc971982b3dcfb88d2ece2f783678670ec36
janianella <angeliranario25@gmail.com>
2026-05-27 22:51:32 +0800

    fix typo database

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.json({ message: 'prism-and-priel API is running' });
+  res.json({ message: 'prism-and-piel API is running' });
 });
 
 module.exports = router;
